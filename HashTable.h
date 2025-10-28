e4fb5f473cbb10d978b23b1efffe24a2f615c979
CriglePig <daniel.141@wright.edu>
2025-10-28 13:23:33 -0400

    Inserted starter code and declarations

--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,27 @@
 /**
  * HashTable.h
  */
+
+class HashTable {
+    public:
+    HashTable(size_t initCapacity); // default constructor
+    bool insert(std::string key, size_t value);
+
+    bool remove(std::string key);
+
+    bool contains(const std::string &key) const;
+
+    std::optional<int> get(const std::string &key) const;
+
+    int &operator[](const std::string &key);
+
+    std::vector<std::string> keys() const;
+
+    double alpha() const;
+
+    size_t capacity() const;
+
+    size_t size() const;
+
+private:
+};
